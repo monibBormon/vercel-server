35b2e29190debe74caf2a6584467f8e2533b2ff8
monibbormon <monibbormon14@gmail.com>
2024-10-15 17:33:18 +0600

    1.1

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 // import { connectDB } from "./config/db.js";
-import cartRouter from "./routes/cartRoute.js";
-import foodRouter from "./routes/foodRoute.js";
-import orderRouter from "./routes/orderRoute.js";
-import userRouter from "./routes/userRoute.js";
 
 // app config
 const app = express();
@@ -12,23 +9,23 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["https://tasty-dash.vercel.app"],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["https://tasty-dash.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 // database connection
 // connectDB();
 
 // api endpoint
-app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads")); // we can access the image at images/image_name now
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
+// app.use("/api/food", foodRouter);
+// app.use("/images", express.static("uploads")); // we can access the image at images/image_name now
+// app.use("/api/user", userRouter);
+// app.use("/api/cart", cartRouter);
+// app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("api working");
